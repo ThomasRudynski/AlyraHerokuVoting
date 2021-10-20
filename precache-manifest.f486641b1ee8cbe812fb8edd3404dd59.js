3b79b5206f942936d35775337e63ec9f19dafefe
ThomasRudynski <thomas.rudynski@gmail.com>
2021-10-20 15:18:52 +0200

    Updates

---
+++ precache-manifest.f486641b1ee8cbe812fb8edd3404dd59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3fd0f3afbfedc52087c01ad40ed418",
+    "url": "/AlyraHerokuVoting/index.html"
+  },
+  {
+    "revision": "d2e307e0e43b64b751aa",
+    "url": "/AlyraHerokuVoting/static/css/2.09699a42.chunk.css"
+  },
+  {
+    "revision": "bcca31c3ac00d86adb98",
+    "url": "/AlyraHerokuVoting/static/css/main.37b9b77c.chunk.css"
+  },
+  {
+    "revision": "d2e307e0e43b64b751aa",
+    "url": "/AlyraHerokuVoting/static/js/2.8ad8fde9.chunk.js"
+  },
+  {
+    "revision": "bcca31c3ac00d86adb98",
+    "url": "/AlyraHerokuVoting/static/js/main.4d411ea8.chunk.js"
+  },
+  {
+    "revision": "18a4607fe2e3a782e7ce",
+    "url": "/AlyraHerokuVoting/static/js/runtime-main.4801933f.js"
+  }
+]);
